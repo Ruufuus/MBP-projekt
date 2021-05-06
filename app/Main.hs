48b51b6c84f5dc4748f804e9ee9cbfb488211963
Ruufuus <rufusikx3@gmail.com>
2021-05-06 20:29:09 +0200

    Add simple encrypt/decrypt function

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,24 @@
 module Main where
 import Control.Monad (void)
 import Data.Maybe
+import Data.ByteString.Char8 (pack)
 import Text.Printf
 
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
-import Lib
+import UtilsCipherChain
 
 main :: IO ()
-main = startGUI defaultConfig setup
+main = do
+    let msg = pack "moje wiadomosc"
+    exampleAES256 msg
+    startGUI defaultConfig setup
+    
 
 setup :: Window -> UI ()
 setup window = void $ do
     return window # set title "Program do szyfrowania wiadomosci"
+
 
     tekst <- UI.input
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,6 @@ module Lib
     ) where
 
 
-import Crypto.Cipher.AES (AES256)
-import Crypto.Cipher.Types (BlockCipher(..), Cipher(..), IV)
-import Crypto.Error (CryptoFailable(..))        
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc2"
