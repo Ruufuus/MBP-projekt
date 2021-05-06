14c3eaca6f7d4a58b2917d555f5f0c7cbb7ef526
Ruufuus <rufusikx3@gmail.com>
2021-05-06 09:46:23 +0200

    Add GUI module + simple GUI to main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
+import Control.Monad (void)
+import Data.Maybe
+import Text.Printf
 
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
 import Lib
 
 main :: IO ()
-main = someFunc
+main = startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup window = void $ do
+    return window # set title "Program do szyfrowania wiadomosci"
+
+    tekst <- UI.input
+
+    getBody window #+ [
+                column [
+                    grid [[string "Wpisz tekst do zaszyfrowania:", element tekst]]
+                ]]
+
+    element tekst
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+
+import Crypto.Cipher.AES (AES256)
+import Crypto.Cipher.Types (BlockCipher(..), Cipher(..), IV)
+import Crypto.Error (CryptoFailable(..))        
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "someFunc2"
